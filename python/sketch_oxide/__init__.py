9a4b11475e24834062a3b8a71450a0282761c3ac
Yury Fedoseev <yfedoseev@gmail.com>
2025-11-26 01:19:40 -0800

    fix: Resolve TypeScript and Python module import issues
    - Fix TypeScript rootDir from './src' to '.' to include test files under __tests__/
    - Add missing class exports to Python __init__.py __all__ list:
      - Membership: CountingBloomFilter, CuckooFilter, RibbonFilter, StableBloomFilter, VacuumFilter, LearnedBloomFilter
      - Quantiles: KllSketch, TDigest
      - Frequency: HeavyKeeper, ElasticSketch, CountSketch, SpaceSaving, SALSA, RemovableUniversalSketch, NitroSketch
      - Similarity: SuperMinHash
      - Range Filters: Grafite, GRF
      - New Sketches: MementoFilter, RatelessIBLT, SlidingHyperLogLog, SplineSketch, QSketch, ExponentialHistogram
      - Universal: UnivMon
    
    These fixes resolve Code Quality job TypeScript compilation errors and mypy module attribute errors.

--- python/sketch_oxide/__init__.py
+++ python/sketch_oxide/__init__.py
@@ -8,14 +8,42 @@ __all__ = [  # noqa: F405
     "ThetaSketch",
     # Membership
     "BinaryFuseFilter",
+    "CountingBloomFilter",
+    "CuckooFilter",
+    "RibbonFilter",
+    "StableBloomFilter",
+    "VacuumFilter",
+    "LearnedBloomFilter",
     # Quantiles
     "DDSketch",
     "ReqSketch",
+    "KllSketch",
+    "TDigest",
     # Frequency
     "CountMinSketch",
     "FrequentItems",
+    "HeavyKeeper",
+    "ElasticSketch",
+    "CountSketch",
+    "SpaceSaving",
+    "SALSA",
+    "RemovableUniversalSketch",
+    "NitroSketch",
     # Similarity
     "MinHash",
+    "SuperMinHash",
+    # Range Filters
+    "Grafite",
+    "GRF",
+    # New Sketches
+    "MementoFilter",
+    "RatelessIBLT",
+    "SlidingHyperLogLog",
+    "SplineSketch",
+    "QSketch",
+    "ExponentialHistogram",
+    # Universal
+    "UnivMon",
     # Version
     "__version__",
 ]
